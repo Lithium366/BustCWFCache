2bbb0d332ad3ca1e3a2facba95007b337dc9a3ca
Ilya Sevrugin <isevrugin@goodrx.com>
2021-01-27 12:53:35 -0800

    bug fix

--- popup.js
+++ popup.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
     .addEventListener("click", function () {
       chrome.tabs.getSelected(null, function (tab) {
         var code =
-          "var hash = Math.random().toString(36).replace(/[^a-z0-9]+/g, ''); if (window.location.search) { if (window.location.search.indexOf('review=') > -1) { window.location.search = window.location.search.replace(/review=([0-9a-z])+/, 'review=' + hash)  } else { window.location.search += '&review=' + hash } } else { window.location.search = 'review=1' + hash }";
+          "var hash = Math.random().toString(36).replace(/[^a-z0-9]+/g, ''); if (window.location.search) { if (/[&?]review=/.test(window.location.search)) { window.location.search = window.location.search.replace(/[&?]review=([0-9A-Za-z])+/, function (match) { return match.replace(/review=(.*)$/, 'review=' + hash) } )  } else { window.location.search += '&review=' + hash } } else { window.location.search = 'review=1' + hash }";
         chrome.tabs.executeScript(tab.id, { code: code });
       });
     });
